57a1c2fe86e7bf2e5b5bfdc68c05e7ecaec5c013
Branan Purvine-Riley <branan@puppetlabs.com>
2012-09-10 11:27:09 -0700

    Make the message even friendlier

--- the_source.c
+++ the_source.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-// Print a friendly message to the world
+// Print an unfriendly message to the world
 int main(int argc, char** argv) {
   puts("Hello, World!\n");
-  puts("It's good to see you today!\n");
+  puts("I hate you so much\n");
   return 0;
 }
