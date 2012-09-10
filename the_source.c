4ab0077a394b409366d8cee21ff2ac1bdf71b8c1
Branan Purvine-Riley <branan@puppetlabs.com>
2012-09-10 12:11:55 -0700

    Don't add extra newlines in output

--- the_source.c
+++ the_source.c
@@ -2,8 +2,8 @@
 
 // Be a dick to the world
 int main(int argc, char** argv) {
-  puts("Hello, World!\n");
-  puts("I love you so much\n");
-  puts("lol j/k. You stink.\n");
+  puts("Hello, World!");
+  puts("I love you so much!");
+  puts("lol j/k. You stink. hahahaha");
   return 0;
 }
